b4616310f32abc27df42b71ed2744372a88d0738
David Blumer <david.blumer@socialbit.de>
2016-03-16 08:50:30 +0100

    added autoScroll to next headline

---
+++ js/scrollToNextHeader.js
@@ -0,0 +1,25 @@
+var headers = $('.jumbotron');
+
+headers.splice(0, 0, $('img')[0]);
+console.log(headers);
+var padding = $(window).height()/4;
+
+var index = 0;
+
+var next = function()
+{
+    if(index >= headers.length)
+    {
+        index = 0
+    }
+    var header = headers[index];
+
+    $('html,body').animate({scrollTop: header.offsetTop},'slow');
+
+    index++;
+};
+
+setInterval(function()
+{
+    next();
+}, 7000);
