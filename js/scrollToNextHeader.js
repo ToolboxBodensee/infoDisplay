48efe00bc35fd1b4c852b5d4fc0a2c8bc16a5ac4
Jonas Otto <jottosmail@gmail.com>
2016-03-17 19:34:31 +0100

    Fixed scrolling

--- js/scrollToNextHeader.js
+++ js/scrollToNextHeader.js
@@ -8,13 +8,13 @@ var index = 0;
 
 var next = function()
 {
-    if(index >= headers.length)
+    if(index >= (headers.length-1))
     {
         index = 0
     }
     var header = headers[index];
 
-    $('html,body').animate({scrollTop: header.offsetTop},'slow');
+    $('html,body').animate({scrollTop: (header.offsetTop - 8)},'slow');
 
     index++;
 };
