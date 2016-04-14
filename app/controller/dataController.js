10060c6f7cb234b1348f65b03c2670e9747f1480
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2016-04-14 16:10:03 +0200

    Moved the autoscroller code into the angular controllers

--- app/controller/dataController.js
+++ app/controller/dataController.js
@@ -1,19 +1,36 @@
 app.controller('DataController', function (
 	  $http
+	, $interval
     , $log
     , $q
     , $rootScope
     , $scope
 )
 {
-	$scope.entries = [];
+	$scope.activeEntry   = null;
+	$scope.entries       = [];
+	$scope.scrollTimeout = {
+		timeout: 1000 * 5,
+		timer:   null
+	};
 
 	$scope.loadData = function()
 	{
 		return $q(function(resolve, reject)
 		{
 			$http.get('data/data.json').then(function(response) {
-				$scope.entries = response.data;
+				$scope.entries     = response.data;
+
+				for (var i = 0; i < $scope.entries.length; ++i)
+				{
+					var currentEntry = $scope.entries[i];
+					currentEntry.id = 'YOLO' + ('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+						var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+						return v.toString(16);
+					}));
+				}
+
+				$scope.activeEntry = $scope.entries[0];
 
 				resolve();
 			}, function ()
@@ -23,8 +40,47 @@ app.controller('DataController', function (
 		});
 	};
 
-	$scope.loadData().then(function()
+	$scope.scrollToEntry = function (entry)
 	{
-		initHeaders();
-	});
+		var jEntry = $('#' + entry.id);
+
+		$log.log('DataController: scrollToEntry', jEntry);
+
+		if (jEntry.length > 0)
+		{
+			var offset = jEntry.offset();
+
+			$log.log('DataController: offset', offset);
+
+			$('html,body').animate({ scrollTop: offset.top - 420 }, 'slow');
+		}
+	};
+
+	$scope.scrollToNextEntry = function ()
+	{
+		var index = $scope.entries.indexOf($scope.activeEntry);
+		++index;
+
+		if (index > $scope.entries.length - 1) {
+			index = 0;
+		}
+
+		$log.log('DataController: scrollToNextEntry, index', index);
+
+		$scope.activeEntry = $scope.entries[index];
+
+		$scope.scrollToEntry($scope.activeEntry);
+	};
+
+	$scope.startScrolling = function ()
+	{
+		if (!$scope.scrollTimeout.timer)
+		{
+			$log.log('DataController: startScrolling');
+
+			$scope.scrollTimeout.timer = $interval($scope.scrollToNextEntry, $scope.scrollTimeout.timeout);
+		}
+	};
+
+	$scope.loadData().then($scope.startScrolling);
 });
